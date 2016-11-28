2dd40371d3d20af761a92fbf636b21df062e0dd8
Slynus <Slynus@github.com>
2016-11-28 13:38:28 +0100

    Arch

---
+++ Gutterballs.part2/src/thread/Client.java
@@ -0,0 +1,141 @@
+package thread;
+
+import model.*;
+import monitor.*;
+import monitorAndThread.*;
+
+public class Client extends Thread {
+
+	private int id;
+	private Chaussure chaussure;
+	private Groupe groupe;
+	private Guichet guichet;
+
+	private boolean isReady;
+
+	private StockChaussure stockChaussure;
+	private PisteJeu pisteJeu;
+	private SalleDanse salleDanse;
+	private Bowling bowling;
+
+	public Client(int id, Guichet guichet, SalleDanse sd, Bowling bl, StockChaussure stock) {
+		this.id = id;
+		this.chaussure = new ChaussureVille();
+		this.guichet = guichet;
+
+		salleDanse = sd;
+
+		isReady = false;
+
+		stockChaussure = stock;
+		bowling = bl;
+	}
+
+	public boolean isReady() {
+		return isReady;
+	}
+
+	public void setIsReady(boolean b) {
+		isReady = b;
+	}
+
+	public Chaussure getChaussure() {
+		return chaussure;
+	}
+
+	public void setChaussure(Chaussure chaussure) {
+		this.chaussure = chaussure;
+	}
+
+	public void setGroupe(Groupe groupe) {
+		this.groupe = groupe;
+	}
+
+	public void setPisteJeu(PisteJeu pj) {
+		pisteJeu = pj;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + "]";
+	}
+
+	public void prevenirBowlingPartieFinit() {
+		bowling.nouvellePisteDispo();
+	}
+
+	public void payer() {
+		guichet.fairePayerClient(this);
+	}
+
+	public void run() {
+		boolean afficherClient = false;
+
+		if (afficherClient) {
+			System.out.println(this + "-> Guichet");
+		}
+
+		guichet.addToGroup(this);// go to guichet et attendre
+
+		groupe.waitGroupeFull(this);
+
+		if (afficherClient) {
+			System.out.println(this + " " + groupe + " à finit d'attendre dans Guichet. -> go StockChaussure ");
+		}
+
+		// go to salle des chaussures
+		stockChaussure.changeVtoB(this); // se chausse
+		
+		
+		groupe.waitAllHaveShoe(this);
+		if (afficherClient) {
+			System.out.println(this + " " + groupe + " à finit d'attendre dans stockChaussure. -> go SalleDeDanse");
+		}
+
+		// go to salle de danse et attend que tout les membres du groupe y soit
+		
+		groupe.waitGroupeSalleDanse(this);
+
+		if (afficherClient) {
+			System.out.println(
+					this + " " + groupe + " à finit d'attendre son groupe dans salleDanse. -> attend piste de jeu");
+		}
+
+		// attends d'etre notifié par soit un membre de son groupe soit par le
+		// bowling qu'une place est libre
+		salleDanse.waitPisteDispo(groupe);
+
+		if (afficherClient) {
+			System.out.println(this + " " + groupe + "  piste de jeu trouver. -> go to pisteDeJeux");
+		}
+
+
+		// attendre que son groupe soit au complet sur la piste puis joue
+		pisteJeu.waitGroupeAndPlay(groupe, this);
+
+		if (afficherClient) {
+			System.out.println(
+					this + " " + groupe + " à finit d'attendre son groupe dans pisteJeux. -> joue une partie");
+		}
+
+		// Jouer :D (le dernier client arriv� dans pisteJeu lance la partie)
+		// Soit prevenir le bowling que la partie est terminée soit il se barre
+		// go to guichet
+		// Payer D:
+		payer();
+
+		if (afficherClient) {
+			System.out.println(this + " a finit de payer -> go StockChaussure");
+		}
+
+		// go to salle des chaussures
+		stockChaussure.changeBtoV(this);
+
+		// go exit
+	}
+
+	public boolean equals(Client cl) {
+		return id == cl.id;
+	}
+
+}
---
+++ Gutterballs.part3/src/monitor/SalleDanse.java
@@ -0,0 +1,62 @@
+package monitor;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import monitorAndThread.Bowling;
+
+public class SalleDanse {
+	private List<Groupe> listGroupe;
+	private Bowling bowling;
+
+	public SalleDanse() {
+		listGroupe = new LinkedList<>();
+	}
+
+	public void setBowling(Bowling b) {
+		bowling = b;
+	}
+
+	// ajoute un groupe seulement s'il n'y est pas déjà
+	public synchronized boolean addGroupe(Groupe grp) {
+		if (!listGroupe.contains(grp)) {
+			listGroupe.add(grp);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public synchronized boolean reserverPiste(Groupe grp) {
+		PisteJeu piste = bowling.getPisteLibre();
+		if (piste != null) {
+			piste.setGroupe(grp);
+			grp.setPisteJeu(piste);
+			listGroupe.remove(grp);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public synchronized void waitPisteDispo(Groupe grp) {
+		while (!(grp.gotPisteJeu())) {
+			// demande si une piste est dispo, si oui la réserve directement
+			if (reserverPiste(grp)) {
+				notifyAll();
+			} else {
+				try {
+					wait();// danse
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public synchronized void nouvellePisteDispo() {
+		notifyAll();
+	}
+
+}
